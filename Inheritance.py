8f459deb58ee4e4562babf8a32b5a92f4c7a3149
hafke88 <hafke88@gmail.com>
2021-08-05 15:24:40 -0400

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,18 @@
+
+
+class Animal:
+    name = ''
+    location = ''
+    weight = ''
+    speed = ''
+
+class BigCat(Animal):
+    teeth = ''
+    pawsize = ''
+
+class Turtle(Animal):
+    shellDimension = ''
+    neckLength= ''
+    
+    
+    
